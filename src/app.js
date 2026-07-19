8fa728c2b73cfc6cfc5561262fd4a2e2b76a52b3
Anton Sosnin <antsosnin@yandex.ru>
2026-07-19 12:06:35 +0300

    Add vertex buffer object for quad rendering and update vertex shader to use attribute for position

--- src/app.js
+++ src/app.js
@@ -640,7 +640,17 @@ gl.vertexAttribPointer(1, 1, gl.FLOAT, false, stride, 2 * 4);
 gl.enableVertexAttribArray(2);
 gl.vertexAttribPointer(2, 2, gl.FLOAT, false, stride, 3 * 4);
 gl.bindVertexArray(null);
-const quadVao = gl.createVertexArray();
+const quadVao = gl.createVertexArray(), quadVbo = gl.createBuffer();
+gl.bindVertexArray(quadVao);
+gl.bindBuffer(gl.ARRAY_BUFFER, quadVbo);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+  -1, -1,  1, -1, -1,  1,
+  -1,  1,  1, -1,  1,  1,
+]), gl.STATIC_DRAW);
+gl.enableVertexAttribArray(0);
+gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
+gl.bindVertexArray(null);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
 const GU = {
   yaw: gl.getUniformLocation(gProg, 'uYaw'),
--- src/shaders.js
+++ src/shaders.js
@@ -65,10 +65,10 @@ void main(){}`;
 
 export const LIGHT_VERTEX_SHADER = `#version 300 es
 precision highp float;
-const vec2 POS[6]=vec2[6](vec2(-1,-1),vec2(1,-1),vec2(-1,1),vec2(-1,1),vec2(1,-1),vec2(1,1));
+layout(location=0) in vec2 aPosition;
 out vec2 vUV;
 void main(){
-  vec2 p=POS[gl_VertexID];
+  vec2 p=aPosition;
   vUV=p*0.5+0.5;
   gl_Position=vec4(p,0.0,1.0);
 }`;
